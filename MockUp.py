87f5a307bdf5e510a3b71e08c6355c42ad909c97
Nathan-Amburgey <amburgey.27@osu.edu>
2022-09-01 17:00:07 -0400

    basic start

--- MockUp.py
+++ MockUp.py
@@ -1,1 +1,7 @@
-# Boxwell-s-box-of-monsters
+# Mock up of the monster generator
+print("Welcome to the monster Library, what difficulty of encouter would you like?")
+input()
+print("Perfect, please enter a party with the format class level for each memeber: ")
+input()
+print("Great ")
+
