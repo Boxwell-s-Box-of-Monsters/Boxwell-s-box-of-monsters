6e3af9b0f50dc499d4dd82148fb41c5013147bc4
Chris Manges <Manges.17@buckeyemail.osu.edu>
2022-09-06 12:15:51 -0400

    Added gui

--- MockUp.py
+++ MockUp.py
@@ -1,7 +1,42 @@
 # Mock up of the monster generator
-print("Welcome to the monster Library, what difficulty of encouter would you like?")
-input()
-print("Perfect, please enter a party with the format class level for each memeber: ")
-input()
-print("Great ")
+import requests
+import json
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("700x500")
+window.title("Monster Generator")
+
+label = tk.Label(window, text="Welcome to the monster library, please enter a party with the format class level for each member.")
+label.pack()
+
+
+variable1 = tk.StringVar(window)
+dropDownMenu1 = tk.OptionMenu(window, variable1, "Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard")
+dropDownMenu1.pack(pady=10)
+variable2 = tk.StringVar(window)
+dropDownMenu2 = tk.OptionMenu(window, variable2, "Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard")
+dropDownMenu2.pack(pady=10)
+variable3 = tk.StringVar(window)
+dropDownMenu3 = tk.OptionMenu(window, variable3, "Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard")
+dropDownMenu3.pack(pady=10)
+variable4 = tk.StringVar(window)
+dropDownMenu4 = tk.OptionMenu(window, variable4, "Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard")
+dropDownMenu4.pack(pady=10)
+
+
+result = tk.StringVar()
+result.set("")
+
+def button_action():
+    response = requests.get("https://www.dnd5eapi.co/api/monsters/troll/")
+    result.set(response.json().get('name'))
+
+button = tk.Button(window, text='Get Monster', command=button_action)
+button.pack(pady=10)
+
+resultLabel = tk.Label(window, textvariable=result)
+resultLabel.pack()
+
+window.mainloop()
 
