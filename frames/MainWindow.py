c020da2ec231ab105650104f7215a9f3d0355f3a
mfengOSU <2013mfstar@gmail.com>
2022-12-06 07:58:36 -0500

    pylint

--- frames/MainWindow.py
+++ frames/MainWindow.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-import numpy as np
 import random
+from os.path import exists
+import numpy as np
 
 from transformers import pipeline
 from PIL import Image, ImageTk
@@ -13,7 +14,6 @@ from frames.DescriptionFrame import DescriptionFrame
 from frames.ResultFrame import ResultFrame
 from frames.ImageInputFrame import ImageInputFrame
 from Image_Generation import ImageGeneration
-from os.path import exists
 
 ############################
 # Main Window
